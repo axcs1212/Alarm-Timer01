a2d094e03387e8692e80d158853beb3f02238575
D7703_25 <d7703_25@D7703-25ui-iMac.local>
2018-04-19 13:12:36 +0900

    alarm

--- Alarm-Timer01/ViewController.swift
+++ Alarm-Timer01/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var myDatePicker: UIDatePicker!
     @IBOutlet weak var alramTime: UILabel!
-    
     @IBOutlet weak var currentTimeLabel: UILabel!
     
     var myTimer = Timer()
@@ -42,13 +41,28 @@ class ViewController: UIViewController {
         let date = Date()
         let formatter = DateFormatter()
         formatter.dateFormat = "hh:mm:ss"
-        
-        // currentTimeLabel 레이블에 표시
         currentTimeLabel.text = formatter.string(from: date)
-        
-        // 빨갱이 만들어주는거
+
         if currentTimeLabel.text == alramTime.text {
             view.backgroundColor = UIColor.red
+            let myAlertController = UIAlertController(title: "알림", message: "설정된 시간이 되었습니다.", preferredStyle: .actionSheet)
+            
+            //AlertAction 만들기
+            let okAction = UIAlertAction(title: "확인", style: .default){(myAction: UIAlertAction) -> Void in
+                self.view.backgroundColor = UIColor.red}
+            let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: { (myAction: UIAlertAction) -> Void in
+                self.view.backgroundColor = UIColor.white})
+            let testAction = UIAlertAction(title: "삭제", style: .destructive, handler:{ (myAction: UIAlertAction) -> Void in
+                self.view.backgroundColor = UIColor.white})
+            self.alramTime.text = "00:00:00"
+            
+            //AlertAction을 AlertController에 넣기
+            myAlertController.addAction(okAction)
+            myAlertController.addAction(cancelAction)
+            myAlertController.addAction(testAction)
+            
+            //화면에 출력
+            present(myAlertController, animated: true, completion: nil)
         }
     }
     
@@ -58,7 +72,8 @@ class ViewController: UIViewController {
         //let myAlramTime = myDatePicker.date
         //레이블에 텍스트를 보여주는 코드
         //alramTime.text = String(describing: myAlramTime)
-        
+        print("change Date Picker")
+        print(myDatePicker.date)
         
         
         //데이터피커의 정보를 받아 실시간으로 밑에 숫자를 뜨게하는거
